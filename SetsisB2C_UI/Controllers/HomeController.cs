3b4110046111cbf8304b9cf4ab5fd6cce3c1ca94
Emirhan Şen <emirhan.sen@setsis.com.tr>
2021-04-06 10:12:14 +0300

    Yeniler

--- SetsisB2C_UI/Controllers/HomeController.cs
+++ SetsisB2C_UI/Controllers/HomeController.cs
@@ -34,16 +34,28 @@ namespace SetsisB2C_UI.Controllers
       
 
         [HttpPost]
+<<<<<<< HEAD
+        public IActionResult Slider(SliderFile sliderFile, string name, string description, string imageLink, int orders, string imageName)
+=======
         public IActionResult Slider (SliderFile sliderFile,string name,string description, string imageLink,int orders,string imageName)
+>>>>>>> c6723686295671da63160cf824ef8aa9c6526e09
         {
             string value = "";
             //value = apiConnect.StrResponse;
             //var userControl = JsonConvert.DeserializeObject<SliderSettings>(value);
+<<<<<<< HEAD
+
+            if (ModelState.IsValid)
+            {
+
+                if (sliderFile.ImageName != null)
+=======
 
             if (ModelState.IsValid)
             {
               
                 if (sliderFile.ImageName !=null)
+>>>>>>> c6723686295671da63160cf824ef8aa9c6526e09
                 {
                     var ex = Path.GetExtension(sliderFile.ImageName.FileName);
                     var newImage = Guid.NewGuid() + ex;
@@ -55,11 +67,19 @@ namespace SetsisB2C_UI.Controllers
                     imageLink = sliderFile.ImageLink;
                     name = sliderFile.Name;
                     orders = sliderFile.Orders;
+<<<<<<< HEAD
+
+                }
+
+
+                ApiConnect apiConnect = new ApiConnect("http://10.20.8.6:2071/Home/AddSlider?name=" + name + "&description=" + description + "&imageName=" + imageName + "&imageLink=" + imageLink + "&orders=" + orders);
+=======
                    
                 }
 
           
                 ApiConnect apiConnect = new ApiConnect("http://10.20.8.6:2071/Home/AddSlider?name="+name+"&description="+description+"&imageName="+imageName+"&imageLink="+imageLink+ "&orders=" + orders);
+>>>>>>> c6723686295671da63160cf824ef8aa9c6526e09
                 return RedirectToAction("Index", "Home");
             }
             return View();
---
+++ SetsisB2C_UI/Models/Brands/Brands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetsisB2C_UI.Models.Brands
+{
+    public class Brands
+    {
+            public string BrandID { get; set; }
+            public string BrandName { get; set; }
+            public string BrandCode { get; set; }
+            public string BrandImgPath { get; set; }
+        
+    }
+}
