819116fa1ed7ea457b2f6e89543a19603d575e58
serkanaydinn <81405477+serkanaydinn@users.noreply.github.com>
2021-04-07 17:49:03 +0300

    Günlük Siparişler,Günlük Kazanç,Günlük İptal apiden çekildi

---
+++ SetsisB2C_UI/Controllers/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SetsisB2B.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetsisB2C_UI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime dateTime = DateTime.Now; 
+            string value = "";
+            string valueCredit = "";
+            string valueCancel = "";
+            ApiConnect apiConnect = new ApiConnect("http://10.20.8.6:2071/Dashboard/GetCountOrder?day=" +dateTime);
+            ApiConnect sumCredit = new ApiConnect("https://localhost:44356/Dashboard/GetCredit?day=2019-05-20 14:06:38.393");
+            ApiConnect getCancel = new ApiConnect("https://localhost:44356/Dashboard/GetCancel?day=2019-04-22 11:26:45.183");
+            value = apiConnect.StrResponse;
+            valueCredit = sumCredit.StrResponse;
+            valueCancel = getCancel.StrResponse;
+
+            ViewData["Count"] = value;
+            ViewData["Sum"] = valueCredit;
+            ViewData["Cancel"] = valueCancel;
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+          
+
+      
+
+
+            return View();
+        }
+        public IActionResult DayReport()
+        {
+
+            return View();
+        }
+    }
+}
